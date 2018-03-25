7484a7014ba2e476650b048b922b9b10c32113fc
emilyageller <emilyageller@gmail.com>
2018-03-24 19:04:57 -0700

    app updates

--- Project 5/app/main.py
+++ Project 5/app/main.py
@@ -3,55 +3,28 @@ from seedling_identifier import identify
 import datetime as dt
 
 app = Flask(__name__)
+appfilepath = '/Users/emilygeller/ds/metis/metisgh/Geller_Metis/Project 5/app'
 
 @app.route('/')
 def entry_page():
-    return render_template('index.html')
-
+    return render_template('index.html', result=False)
 
 
 @app.route('/identify/', methods = ['POST'])
 def upload_file():
    if request.method == 'POST':
       now = dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-      filepath = '/Users/emilygeller/ds/metis/metisgh/Geller_Metis/Project 5/app/uploads/user_upload' + now + '.png'
+      imagepath = '/static/uploads/user_upload' + now + '.png'
+      filepath = appfilepath + imagepath
       f = request.files['file']
       f.save(filepath)
-      return identify(filepath)
-        
-
-
-
-# @app.route('/identify/', methods=['POST'])
-# def render_message():
-#     print(request.form)
-
-
-#     # output = identify(
-        
-#     #     )
-    
-    
-
-#     if output[0][0] < 0.5:
-#         message = 'Domestic'
-#     else:
-#         message = 'International'
-
-#     percent_chance_international = 100 * output[0][0]
-#     percent_chance_domestic = 100 * output[0][1]
-
-#     return render_template(
-#         'index.html', 
-#         result=True, 
-#         message=message, 
-#         form_values=request.form,
-#         domestic_chance=percent_chance_domestic,
-#         international_chance=percent_chance_international
-
-#         )
-
-
+      message = identify(filepath)
+      return render_template(
+        'index.html',
+        result= True,
+        message=message,
+        imagepath=imagepath)
+      
 
 if __name__ == '__main__':
     app.run(debug=False)
